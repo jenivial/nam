7bc7529c1d45dec0c9d4f7714e12dd47845c6786
Jesús Eduardo Nivia Latorre <jenivial@unal.edu.co>
2023-08-21 20:05:02 -0500

    Initial solution

---
+++ NamApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NamApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NamApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NamApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
